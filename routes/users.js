562e46359f4eec13a39cc1d6954f8ecdb197fb87
Steven <hspichardo@gmail.com>
2020-05-13 17:31:40 +0200

    Add CRUDS operations to users and test #9

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,76 @@ const { check, validationResult } = require('express-validator');
 const User = require('../models/user')
 const router = express.Router();
 const httpCodes = require('../resources/httpCodes')
+router.get('/', async (req, res) => {
+    const users = await User.find();
+    res.status(httpCodes.codes.OK).json(users);
+});
 
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id)
+    if(!user) return res.status(httpCodes.codes.NOTFOUND).send('User not found in DB')
+    res.send(user);
+});
 
+router.post('/', [
+    check('dni').isLength({min: 3}),
+    check('email').isLength({min: 3},
+        check('password').isLength({min: 8}))
+],async(req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(httpCodes.codes.FORBIDDEN).json({ errors: errors.array() });
+    }
+    let user = (await User.findOne({email: req.body.email}) || await User.findOne({dni: req.body.dni}) )
+    if(user) return res.status(httpCodes.codes.CONFLICT).json({message: "Usuario ya existe"})
 
-/* GET users listing. */
+    user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        dni: req.body.dni,
+        password: req.body.password,
+        roles: req.body.roles
+    })
 
+    const result = await user.save()
+    res.status(httpCodes.codes.CREATED).send(result)
+});
 
+router.put('/:id', [
+    check('name').isLength({min: 3}),
+    check('email').isLength({min: 3})
+], async (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(httpCodes.codes.FORBIDDEN).json({ errors: errors.array() });
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.id,{
+            name: req.body.name,
+            dni: req.body.dni,
+            email: req.body.email,
+            roles: req.body.roles
+        },
+        {
+            new: true
+        })
+
+    if(!user){
+        return res.status(httpCodes.codes.NOTFOUND).send('El usuario con ese ID no esta')
+    }
+
+    res.status(httpCodes.codes.NOCONTENT).send()
+})
+
+router.delete('/:id', async(req, res)=>{
+
+    const user = await User.findByIdAndDelete(req.params.id)
+
+    if(!user){
+        return res.status(httpCodes.codes.NOTFOUND).send('El user con ese ID no esta, no se puede borrar')
+    }
+
+    res.status(httpCodes.codes.OK).send({message: "Usuario eliminado correctamente"})
+
+});
 module.exports = router;
--- test/testRestApi.js
+++ test/testRestApi.js
@@ -11,15 +11,68 @@ const assert = chai.assert;
 
 chai.use(chaiHttp);
 const url ='http://localhost:' + (process.env.PORT || '3000');
-
+var id;
 describe('Testing toberumanAPI managing: CHAI + REST', function () {
     it('should return an Hola Mundo message', function(done){
         chai.request(url)
             .get("/")
             .end(function(err, res){
                 expect(res).to.have.status(httpCodes.codes.OK);
-                assert.equal(res.text,'Hola Mundo');
+                expect(res.body).to.have.property('message','Hola Mundo');
                 done();
             });
     });
+
+    it('should return an array of users', function (done) {
+        chai.request(url)
+            .get("/users")
+            .end(function (err,res) {
+                expect(res).to.have.status(httpCodes.codes.OK);
+                expect(res.body).to.be.a('array');
+                done();
+            });
+    });
+
+    it('should create a new user', function (done) {
+        chai.request(url)
+            .post("/users")
+            .send({name: "usuarioprueba",email: "prueba@prueba.com",dni:"0315649866",password: "hola12345"})
+            .end(function (err,res) {
+                expect(res).to.have.status(httpCodes.codes.CREATED);
+                expect(res.body).to.be.a('object');
+                id = res.body._id;
+                done();
+            });
+    });
+
+    it('should get an specific user', function () {
+        chai.request(url)
+            .get("/users/"+id)
+            .end((err,res)=>{
+                expect(res).to.have.status(httpCodes.codes.OK);
+                expect(res.body).to.have.property('name','usuarioprueba');
+                done();
+            })
+    });
+    it('should update an user', function () {
+        chai.request(url)
+            .put("/users/"+id)
+            .send({name: "usuariopruebaUPD",email: "prueba2@prueba.com",dni:"0315649466"})
+            .end((err,res)=>{
+                expect(res).to.have.status(httpCodes.codes.NOCONTENT);
+                expect(res.body).to.be.empty;
+                done();
+            })
+    });
+
+    it('should delete a user', function () {
+        chai.request(url)
+            .delete("/users/"+id)
+            .end((err,res)=> {
+                expect(res).to.have.status(httpCodes.codes.OK);
+                done();
+            })
+    });
 });
+
+
