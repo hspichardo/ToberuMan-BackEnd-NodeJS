3f5c85003c980bf3f98aabdac119f0029c1e65d3
Steven <hspichardo@gmail.com>
2020-05-13 16:44:40 +0200

    create user module #6

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/user')
+const router = express.Router();
+const httpCodes = require('../resources/httpCodes')
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
 
 module.exports = router;
