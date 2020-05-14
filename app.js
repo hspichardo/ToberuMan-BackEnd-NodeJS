bb6c4df7822f7e9331dd51514d633455dfc538c4
Steven <hspichardo@gmail.com>
2020-05-14 15:50:51 +0200

    add auth controller and post for login with dni and encrypted password #12

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 const mongodb = require('./db/mongocon')
 const bodyParser = require('body-parser');
 const app = express();
@@ -16,5 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -4,3 +4,20 @@ const { check, validationResult } = require('express-validator');
 const User = require('../models/user')
 const router = express.Router();
 const httpCodes = require('../resources/httpCodes');
+
+router.post('/', [
+    check('dni').isLength({min: 3}),
+    check('password').isLength({min: 8})
+],async(req, res)=> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(httpCodes.codes.FORBIDDEN).json({errors: errors.array()});
+    }
+    let user = await User.findOne({dni: req.body.dni});
+    if (!user) return res.status(httpCodes.codes.NOTFOUND).json({message: "Usuario y contraseña incorrectos"});
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(httpCodes.codes.NOTFOUND).json({message: "Usuario y contraseña incorrectos"});
+    res.send('Usuario y contraseña correcta');
+});
+
+module.exports = router;
